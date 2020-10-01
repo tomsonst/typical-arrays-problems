14bdcb50d3b4790a010819d3764f1e00af3b5647
tomsonst <tomaschovst@gmail.com>
2020-10-01 21:24:44 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+    try {let result = 0;
+    for(let i=0; i<arr.length; i++){
+      if(arr[i] < result){
+        result = arr[i];
+      }
+    }
+   return(result);
+  } catch(e){
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  try{
+    let result = 0;
+    for(let i=0; i<arr.length; i++){
+      if(arr[i] > result){
+        result = arr[i];
+      }
+    }
+    return(result);
+  } catch(e){
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  try{
+    let result = 0;
+    if(arr.length === 0){return 0;}
+    for(let i=0; i<arr.length; i++){
+      result += arr[i];
+    }
+    return(result/arr.length);
+  } catch(e){
+    return 0;
+  }
 }
